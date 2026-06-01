0568016f015d9cbf416d0553d3ee9e08ec9dabb6
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2026-05-31 21:30:21 -0300

    corrige router duplicado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
-
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <AppRoutes />
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
