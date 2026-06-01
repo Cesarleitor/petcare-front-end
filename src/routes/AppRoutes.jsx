4329883964c9214de301a4404507acab3b3cc1da
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2026-05-31 21:07:55 -0300

    ajustes hero responsividade e deploy

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home/Home";
 import Services from "../pages/Services/Services";
@@ -8,13 +8,15 @@ import Contact from "../pages/Contact/Contact";
 
 function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/servicos" element={<Services />} />
-      <Route path="/adocao" element={<Adoption />} />
-      <Route path="/sobre" element={<About />} />
-      <Route path="/contato" element={<Contact />} />
-    </Routes>
+    <BrowserRouter basename="/petcare">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/servicos" element={<Services />} />
+        <Route path="/adocao" element={<Adoption />} />
+        <Route path="/sobre" element={<About />} />
+        <Route path="/contato" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
