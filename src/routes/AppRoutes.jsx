642d962804d993d5bc418e07cdcb4baf084186fb
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2026-05-31 21:36:20 -0300

    corrige router github pages

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home/Home";
 import Services from "../pages/Services/Services";
@@ -8,15 +8,13 @@ import Contact from "../pages/Contact/Contact";
 
 function AppRoutes() {
   return (
-    <BrowserRouter basename="/petcare">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/servicos" element={<Services />} />
-        <Route path="/adocao" element={<Adoption />} />
-        <Route path="/sobre" element={<About />} />
-        <Route path="/contato" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/servicos" element={<Services />} />
+      <Route path="/adocao" element={<Adoption />} />
+      <Route path="/sobre" element={<About />} />
+      <Route path="/contato" element={<Contact />} />
+    </Routes>
   );
 }
 
