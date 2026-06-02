00e223085aa457fa67e16de7426b14540c6b9415
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2026-06-02 12:28:31 -0300

    style: padroniza banners internos com overlay institucional

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,5 +1,23 @@
+import pet3 from "../../assets/pet3.png";
+import "./Services.css";
+
 function Services() {
-  return <h1>Services</h1>;
+  return (
+    <main className="services">
+      <section className="services-hero">
+  <img src={pet3} alt="Pets felizes na PetCare" />
+
+  <div className="services-overlay">
+    <h1>Nossos Serviços</h1>
+
+    <p>
+      Tudo o que seu pet precisa para viver com saúde,
+      conforto e felicidade.
+    </p>
+  </div>
+</section>
+    </main>
+  );
 }
 
 export default Services;
